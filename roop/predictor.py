c44926e43ca0dd18492f1395b214b27ac48dcd8d
VISHAL SHARMA <57282548+nick-arch@users.noreply.github.com>
2024-05-14 15:09:10 +0530

    Update predictor.py

--- roop/predictor.py
+++ roop/predictor.py
@@ -1,43 +1,24 @@
-import threading
-import numpy
 import opennsfw2
 from PIL import Image
-from keras import Model
-
 from roop.typing import Frame
 
 PREDICTOR = None
-THREAD_LOCK = threading.Lock()
-MAX_PROBABILITY = 0.85
 
-
-def get_predictor() -> Model:
+def get_predictor() -> opennsfw2.Model:
     global PREDICTOR
-
-    with THREAD_LOCK:
-        if PREDICTOR is None:
-            PREDICTOR = opennsfw2.make_open_nsfw_model()
+    if PREDICTOR is None:
+        PREDICTOR = opennsfw2.make_open_nsfw_model()
     return PREDICTOR
 
-
-def clear_predictor() -> None:
-    global PREDICTOR
-
-    PREDICTOR = None
-
-
-def predict_frame(target_frame: Frame) -> bool:
+def predict_frame(target_frame: Frame) -> float:
     image = Image.fromarray(target_frame)
     image = opennsfw2.preprocess_image(image, opennsfw2.Preprocessing.YAHOO)
     views = numpy.expand_dims(image, axis=0)
-    _, probability = get_predictor().predict(views)[0]
-    return probability > MAX_PROBABILITY
+    return get_predictor().predict(views)[0][1]  # Return the probability directly
 
+def predict_image(target_path: str) -> float:
+    return opennsfw2.predict_image(target_path)  # Return the probability directly
 
-def predict_image(target_path: str) -> bool:
-    return opennsfw2.predict_image(target_path) > MAX_PROBABILITY
-
-
-def predict_video(target_path: str) -> bool:
+def predict_video(target_path: str) -> list[float]:
     _, probabilities = opennsfw2.predict_video_frames(video_path=target_path, frame_interval=100)
-    return any(probability > MAX_PROBABILITY for probability in probabilities)
+    return probabilities  # Return a list of probabilities for each frame
